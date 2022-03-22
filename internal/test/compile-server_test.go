60a1067e19f4fd676f95bc803e89a99603c49504
moyu <279972161@qq.com>
2022-03-22 22:13:23 +0800

    测试连接非本地的编译中心进行获得可执行文件

--- internal/test/compile-server_test.go
+++ internal/test/compile-server_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	v1 "gitee.com/moyusir/compilation-center/api/compilationCenter/v1"
 	utilApi "gitee.com/moyusir/util/api/util/v1"
+	g "github.com/go-kratos/kratos/v2/transport/grpc"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"os"
@@ -13,7 +14,13 @@ import (
 
 func TestCompilationCenter(t *testing.T) {
 	// 启动服务器，获得客户端
-	client := StartCompilationCenterServer(t)
+	//client := StartCompilationCenterServer(t)
+	conn, err := g.DialInsecure(context.Background(),
+		g.WithEndpoint("compilation-center.test.svc.cluster.local:9000"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := v1.NewBuildClient(conn)
 
 	// 定义注册信息
 	configInfo := []*utilApi.DeviceConfigRegisterInfo{
